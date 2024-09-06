dfc376e0f3d71a650913a734ea875aadc9fb536f
Andreas Johnsen <secretemail>
2024-09-06 15:56:20 +0200

    sorting and autodeploy

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ const tableElement = document.createElement('table');
 tableElement.classList.add('table-high-score');
 document.body.appendChild(tableElement);
 
-Object.entries(highscoreMap).forEach(([initials, score], index) => {
+Object.entries(highscoreMap).sort(([_, a], [__, b]) => b - a).forEach(([initials, score], index) => {
     const tableRowElement = document.createElement('tr');
     tableRowElement.classList.add(`score-row-${index}`);
     tableElement.appendChild(tableRowElement);
